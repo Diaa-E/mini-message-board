072981f08d36eb01f42ca901ef159f871386467a
Diaa-E <blarb21@yahoo.com>
2025-08-30 21:16:36 +0300

    Add sent messages

--- routes/indexRouter.js
+++ routes/indexRouter.js
@@ -2,9 +2,27 @@ import { Router } from "express";
 
 const indexRouter = Router();
 
+const messages = [
+    {
+        text: "Message 1",
+        user: "Bruce",
+        added: new Date()
+    },
+    {
+        text: "Message 2",
+        user: "Clark",
+        added: new Date()
+    },
+    {   
+        text: "Message 3",
+        user: "Diana",
+        added: new Date()
+    }
+];
+
 indexRouter.get("/", (req, res) => {
 
-        res.render("index");
+        res.render("index", { messages: messages });
     })
     .get("/{*splat}", (req, res) => {
 
