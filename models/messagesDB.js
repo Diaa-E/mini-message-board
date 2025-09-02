630685f2ec1ca9b0e364df551de52472ce9cc359
Diaa-E <blarb21@yahoo.com>
2025-09-02 19:21:16 +0300

    Replace placeholder messages with more organic ones - Clip user name overflow

--- models/messagesDB.js
+++ models/messagesDB.js
@@ -2,19 +2,67 @@ import { v4 as uuid } from "uuid";
 
 const messages = [
     {
-        text: "Message 1",
+        text: "Hey! Are you free this weekend?",
         user: "Bruce",
         added: new Date(),
         id: uuid()
     },
     {
-        text: "Message 2",
+        text: "Guess what? I aced my exam!",
         user: "Clark",
         added: new Date(),
         id: uuid()
     },
     {   
-        text: "Message 3",
+        text: "Just having one of those days, you know? Let's grab coffee and talk it out.",
+        user: "Diana",
+        added: new Date(),
+        id: uuid()
+    },
+    {
+        text: "This is 🔥! Thank you!",
+        user: "John",
+        added: new Date(),
+        id: uuid()
+    },
+    {
+        text: "Don't worry, we all have those stories. It's what makes life interesting!",
+        user: "Christine",
+        added: new Date(),
+        id: uuid()
+    },
+    {
+        text: "Just saw your favorite band is coming to town! We have to get tickets ASAP!",
+        user: "Joanna",
+        added: new Date(),
+        id: uuid()
+    },
+    {
+        text: "How was your day?",
+        user: "Bruce",
+        added: new Date(),
+        id: uuid()
+    },
+    {
+        text: "I'm on it. Front row seats, here we come!",
+        user: "Peter",
+        added: new Date(),
+        id: uuid()
+    },
+    {
+        text: "Great, how was yours?",
+        user: "Clark",
+        added: new Date(),
+        id: uuid()
+    },
+    {
+        text: "Need some advice. Can we chat?",
+        user: "Miranda",
+        added: new Date(),
+        id: uuid()
+    },
+    {
+        text: "Guess who I bumped into today?",
         user: "Diana",
         added: new Date(),
         id: uuid()
