65e2905d43c31f799068581e0b90ae66fa5db81b
Diaa-E <blarb21@yahoo.com>
2025-09-03 00:36:39 +0300

    Add an error page

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.use("/", indexRouter);
 app.use((err, req, res, next) => {
 
     console.error(err);
-    res.status(err.statusCode || 500).send(err.message);
+    res.status(err.statusCode || 500).render("error", { statusCode: err.statusCode || 500, message: err.message });
 });
 
 const PORT = process.env.PORT || 3000;
