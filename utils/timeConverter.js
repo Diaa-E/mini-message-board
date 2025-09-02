b0cbec02aa01b3e468375da394b389fd9c14a295
Diaa-E <blarb21@yahoo.com>
2025-09-02 20:04:40 +0300

    Write a plural time unit only when unit is more than 1

--- utils/timeConverter.js
+++ utils/timeConverter.js
@@ -2,26 +2,43 @@ export function convertMs(timeMs)
 {
     if (timeMs < 60 * 1000)
     {
-        return `${Math.floor(timeMs / 1000)} seconds`;
+        const timeSec = Math.floor(timeMs / 1000);
+
+        return `${timeSec} second${makePlural(timeSec)}`;
     }
     else if (timeMs < 60 * 60 * 1000)
     {
-        return `${Math.floor(timeMs / (1000 * 60))} minutes`;
+        const timeMin = Math.floor(timeMs / (1000 * 60));
+
+        return `${timeMin} minute${makePlural(timeMin)}`;
     }
     else if (timeMs < 60 * 60 * 24 * 1000)
     {
-        return `${Math.floor(timeMs / (1000 * 60 * 60 ))} hours`;
+        const timeHr = Math.floor(timeMs / (1000 * 60 * 60 ));
+
+        return `${timeHr} hour${makePlural(timeHr)}`;
     }
     else if (timeMs < 60 * 60 * 24 * 30 * 1000)
     {
-        return `${Math.floor(timeMs / (1000 * 60 * 60 * 24 ))} days`
+        const timeDay = Math.floor(timeMs / (1000 * 60 * 60 * 24 ));
+
+        return `${timeDay} day${makePlural(timeDay)}`;
     }
     else if (timeMs < 60 * 60 * 24 * 30 * 12 * 1000)
     {
-        return `${Math.floor(timeMs / (1000 * 60 * 60 * 24 * 30 ))} months`
+        const timeMon = Math.floor(timeMs / (1000 * 60 * 60 * 24 * 30 ));
+
+        return `${timeMon} month${makePlural(timeMon)}`;
     }
     else
     {
-        return  `${Math.floor(timeMs / (1000 * 60 * 60 * 24 * 30 ))} years`;
+        const timeYr = Math.floor(timeMs / (1000 * 60 * 60 * 24 * 30 ));
+
+        return  `${timeYr} year${makePlural(timeYr)}`;
     }
+}
+
+function makePlural(quantity)
+{
+    return quantity > 1 ? "s" : "";
 }
